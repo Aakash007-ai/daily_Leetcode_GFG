f84b8058a6300ea3d2b0322505c1f16811246bf6
Aakash Phogat <aakash.phogat@zupee.in>
2025-08-25 06:37:11 +0530

    striver sheet on mac

--- striver/striver_Sde_Sheet/day_1/nextPermutation.cpp
+++ striver/striver_Sde_Sheet/day_1/nextPermutation.cpp
@@ -1,6 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// next permutation 
+// it is just a next lexicographically character
+/**
+
+*/
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
